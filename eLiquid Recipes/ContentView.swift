9575868c40eabd901fc1a6250369c28ddecc7e43
hugosilvag6 <hugosilvag6@hotmail.com>
2022-08-26 21:08:48 -0300

    Initial Commit

---
+++ eLiquid Recipes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eLiquid Recipes
+//
+//  Created by Hugo Silva on 26/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eLiquid Recipes/eLiquid_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  eLiquid_RecipesApp.swift
+//  eLiquid Recipes
+//
+//  Created by Hugo Silva on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct eLiquid_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
